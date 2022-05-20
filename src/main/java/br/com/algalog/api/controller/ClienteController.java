c558217c2390a9bb0884cb20f49056322d857860
padme95 <emily-ss@hotmail.com>
2022-05-20 00:13:34 -0300

    Atualização

--- src/main/java/br/com/algalog/api/controller/ClienteController.java
+++ src/main/java/br/com/algalog/api/controller/ClienteController.java
@@ -2,9 +2,17 @@ package br.com.algalog.api.controller;
 
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.algalog.domain.model.Cliente;
@@ -27,4 +35,44 @@ public class ClienteController {
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> getOne(@PathVariable Long id){
+		return clienteRepository.findById(id)
+		//.map(cliente -> ResponseEntity.ok(cliente)) da no mesmo que a linha debaixo
+			.map(ResponseEntity::ok)
+			.orElse(ResponseEntity.notFound().build());
+		
+//		Optional<Cliente> cliente = clienteRepository.findById(id);
+//		
+//		if(cliente.isPresent()) {
+//			return ResponseEntity.ok(cliente.get());
+//		}
+//		
+//		return ResponseEntity.notFound().build();
+ 	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente){
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Cliente> atuaizar(@PathVariable Long id,
+		@RequestBody Cliente cliente) {
+		if (!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(id);
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
+		
+	}
 }
+
+
+
+
